c8d89c3963e48e19cb7e25f2da19f38ac7931631
daniil <daniil_baldin@ukr.net>
2023-03-27 13:32:56 +0300

    Third commit(canvas edit to make added text draggable)

--- src/components/pages/components/canvas/Canvas.tsx
+++ src/components/pages/components/canvas/Canvas.tsx
@@ -5,6 +5,14 @@ import { textSelector } from '~/store/selectors/textSelector';
 
 import { CanvasMain } from './canvasStyles';
 
+type Text = {
+    text: string;
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+};
+
 export const Canvas = () => {
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
@@ -12,12 +20,19 @@ export const Canvas = () => {
         event.preventDefault();
     };
 
+    const texts: Text[] = [];
+
     const state = Selector(imageSelector);
     const stateText = Selector(textSelector);
     const lastText = stateText.slice(-1);
     const lastItem = state.slice(-1);
     const source = lastItem[0].image.src.split('/').reverse()[0];
     const text = lastText[0].texts.text;
+    texts.push({ text: text, x: 10, y: 30, width: 100, height: 16 });
+
+    let selectedText = -1;
+    let startX: number;
+    let startY: number;
 
     const handleDrop = (e: React.DragEvent<HTMLCanvasElement> | undefined) => {
         if (!e) {
@@ -47,13 +62,73 @@ export const Canvas = () => {
     };
 
     const handleText = () => {
-        const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-        const ctx = canvas.getContext('2d');
-        if (!ctx) return;
+        const canvas = canvasRef.current;
+        if (!canvas) {
+            return;
+        }
+        const context = canvas.getContext('2d');
+        if (!context) {
+            return;
+        }
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        for (let i = 0; i < texts.length; i++) {
+            const t = texts[i];
+            context.font = '24px serif';
 
-        ctx.font = '24px serif';
+            context.fillText(t.text, t.x, t.y);
+        }
+    };
 
-        ctx.fillText(text, canvas.width / 2, canvas.height / 2);
+    const textHit = (x: number, y: number, textIndex: number) => {
+        const t = texts[textIndex];
+        return x >= t.x && x <= t.x + t.width && y >= t.y - t.width && y <= t.y;
+    };
+
+    const handleMouseDown = (event: React.MouseEvent<HTMLCanvasElement> | undefined) => {
+        if (!event) return;
+        event.preventDefault();
+        const canvas = canvasRef.current;
+        if (!canvas) return;
+        startX = event.clientX - canvas.offsetLeft;
+        startY = event.clientY - canvas.offsetTop;
+
+        for (let i = 0; i < texts.length; i++) {
+            if (textHit(startX, startY, i)) {
+                selectedText = i;
+            }
+        }
+    };
+
+    const handleMouseUp = (e: React.MouseEvent<HTMLCanvasElement> | undefined) => {
+        if (!e) return;
+        e.preventDefault();
+        selectedText = -1;
+    };
+
+    const handleMouseOut = (e: React.MouseEvent<HTMLCanvasElement> | undefined) => {
+        if (!e) return;
+        e.preventDefault();
+        selectedText = -1;
+    };
+
+    const handleMouseMove = (e: React.MouseEvent<HTMLCanvasElement> | undefined) => {
+        if (!e) return;
+        if (selectedText < 0) return;
+        e.preventDefault();
+        const canvas = canvasRef.current;
+        if (!canvas) return;
+        const mouseX = e.clientX - canvas.offsetLeft;
+        const mouseY = e.clientY - canvas.offsetTop;
+
+        const dX = mouseX - startX;
+        const dY = mouseY - startY;
+        startX = mouseX;
+        startY = mouseY;
+
+        const t = texts[selectedText];
+        t.x += dX;
+        t.y += dY;
+        handleText();
     };
 
     useEffect(() => {
@@ -61,6 +136,17 @@ export const Canvas = () => {
     }, [text]);
 
     return (
-        <CanvasMain id="canvas" width={1200} height={700} ref={canvasRef} onDragOver={enableDrop} onDrop={handleDrop} />
+        <CanvasMain
+            id="canvas"
+            width={1200}
+            height={700}
+            ref={canvasRef}
+            onDragOver={enableDrop}
+            onDrop={handleDrop}
+            onMouseDown={handleMouseDown}
+            onMouseOut={handleMouseOut}
+            onMouseUp={handleMouseUp}
+            onMouseMove={handleMouseMove}
+        />
     );
 };
